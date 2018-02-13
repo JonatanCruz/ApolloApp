369f5a3e44fddf86bd21943407b7966fcb3d2b81
JonatanCruz <tlilayatzi.jonatan@outlook.com>
2018-02-13 00:07:38 -0600

    Agregar archivos de proyecto.

---
+++ Apollo/Apollo/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Apollo.Views
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
